6884672c6f90dedb60c3fb90ccaa98f0b3809822
Geoffrey <geo.geofe@gmail.com>
2017-05-14 23:51:32 -0500

    problem #5 made the for loop in oneline

--- list.py
+++ list.py
@@ -1,7 +1,4 @@
 numbers1=[1,2,3,4,5,43,67,58,81]
 numbers2=[6,8,93,12,8,90,43,67,58]
-comman=list()
-for number in numbers1:
-  if(number in numbers2):
-    comman.append(number)
+comman=[number for number in numbers1 if number in numbers2 ]
 print("The number comman  in both the list are {} ".format(comman))
