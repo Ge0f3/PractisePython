d79c3da86785fb5e8a9d522c457f1ac96536264c
Geoffrey <geo.geofe@gmail.com>
2017-05-14 23:23:57 -0500

    problem 5 list overlap

---
+++ list.py
@@ -0,0 +1,7 @@
+numbers1=[1,2,3,4,5,43,67,58,81]
+numbers2=[6,8,93,12,8,90,43,67,58]
+comman=list()
+for number in numbers1:
+  if(number in numbers2):
+    comman.append(number)
+print("The number comman  in both the list are {} ".format(comman))
